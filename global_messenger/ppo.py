04fd55cdd738f6d5cc517e8271842312e8eeff89
nishantkr18 <nniishantkumar@gmail.com>
2021-01-05 23:33:28 +0530

    global and local agent updates complete

--- global_messenger/ppo.py
+++ global_messenger/ppo.py
@@ -27,7 +27,7 @@ class Memory:
 
 
 class ActorCritic(nn.Module):
-    def __init__(self, state_dim, action_dim, action_std, hidden_nodes=64):
+    def __init__(self, state_dim, action_dim, n_agents, action_std, hidden_nodes=64):
         super(ActorCritic, self).__init__()
         # action mean range -1 to 1
         self.actor = nn.Sequential(
@@ -35,7 +35,7 @@ class ActorCritic(nn.Module):
             nn.Tanh(),
             nn.Linear(hidden_nodes, hidden_nodes),
             nn.Tanh(),
-            nn.Linear(hidden_nodes, action_dim),
+            nn.Linear(hidden_nodes, action_dim * n_agents),
             nn.Tanh()
         )
         # critic
@@ -44,8 +44,10 @@ class ActorCritic(nn.Module):
             nn.Tanh(),
             nn.Linear(hidden_nodes, hidden_nodes),
             nn.Tanh(),
-            nn.Linear(hidden_nodes, 1)
+            nn.Linear(hidden_nodes, n_agents)
         )
+        self.n_agents = n_agents
+        self.action_dim = action_dim
         self.action_var = torch.full((action_dim,), action_std * action_std).to(device)
 
     def forward(self):
@@ -53,44 +55,50 @@ class ActorCritic(nn.Module):
 
     def act(self, state):
         action_mean = self.actor(state)
+        action_mean = action_mean.reshape(-1, self.n_agents, self.action_dim)
         cov_mat = torch.diag(self.action_var).to(device)
+        action = []
+        action_logprob = []
+        for i in range(self.n_agents):
+            dist = MultivariateNormal(action_mean[:, i, :], cov_mat)
+            a = dist.sample()
+            action.append(a)
+            action_logprob.append(dist.log_prob(a))
 
-        dist = MultivariateNormal(action_mean, cov_mat)
-        action = dist.sample()
-        action_logprob = dist.log_prob(action)
+        return torch.stack(action).detach(), torch.stack(action_logprob).detach()
 
-        return action.detach(), action_logprob.detach()
-
-    def evaluate(self, state, action):
-        action_mean = self.actor(state)
-
-        action_var = self.action_var.expand_as(action_mean)
+    def evaluate(self, state, action, i):
+        action_mean = self.actor(state.float())
+        action_mean = action_mean.reshape(-1, self.n_agents, self.action_dim)
+        action_var = self.action_var.expand_as(action_mean[:, i, :])
         cov_mat = torch.diag_embed(action_var).to(device)
 
-        dist = MultivariateNormal(action_mean, cov_mat)
+        dist = MultivariateNormal(action_mean[:, i, :], cov_mat)
 
         action_logprobs = dist.log_prob(action)
         dist_entropy = dist.entropy()
-        state_value = self.critic(state)
+        state_value = self.critic(state.float())
 
-        return action_logprobs, torch.squeeze(state_value), dist_entropy
+        return action_logprobs, torch.squeeze(state_value[:, i]), dist_entropy
 
 
 class PPO:
-    def __init__(self, state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip, hidden_nodes=64):
+    def __init__(self, state_dim, action_dim, n_agents, action_std, lr, betas, gamma, K_epochs, eps_clip, hidden_nodes=64):
         self.lr = lr
         self.betas = betas
         self.gamma = gamma
         self.eps_clip = eps_clip
         self.K_epochs = K_epochs
 
-        self.policy = ActorCritic(state_dim, action_dim, action_std, hidden_nodes).to(device)
+        self.policy = ActorCritic(state_dim, action_dim, n_agents, action_std, hidden_nodes).to(device)
         self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr, betas=betas)
 
-        self.policy_old = ActorCritic(state_dim, action_dim, action_std, hidden_nodes).to(device)
+        self.policy_old = ActorCritic(state_dim, action_dim, n_agents, action_std, hidden_nodes).to(device)
         self.policy_old.load_state_dict(self.policy.state_dict())
 
         self.MseLoss = nn.MSELoss()
+        self.action_dim = action_dim
+        self.n_agents = n_agents
 
     def select_action(self, state):
         state = torch.FloatTensor(state.reshape(1, -1)).to(device)
@@ -102,141 +110,45 @@ class PPO:
 
     def update(self, memory):
         # Monte Carlo estimate of rewards:
-        rewards = []
-        discounted_reward = 0
-        for reward, is_terminal in zip(reversed(memory.rewards), reversed(memory.is_terminals)):
-            if is_terminal:
-                discounted_reward = 0
-            discounted_reward = reward + (self.gamma * discounted_reward)
-            rewards.insert(0, discounted_reward)
+        rew = np.array(memory.rewards)
+        is_ter = np.array(memory.is_terminals)
+        for i in range(self.n_agents):
+            rewards = []
+            discounted_reward = 0
+            
+            for reward, is_terminal in zip(reversed(rew[:, i]), reversed(is_ter[:, i])):
+                if is_terminal:
+                    discounted_reward = 0
+                discounted_reward = reward + (self.gamma * discounted_reward)
+                rewards.insert(0, discounted_reward)
 
-        # Normalizing the rewards:
-        rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+            # Normalizing the rewards:
+            rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
+            rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
 
-        # convert list to tensor
-        old_states = torch.squeeze(torch.stack(memory.states).to(device), 1).detach()
-        old_actions = torch.squeeze(torch.stack(memory.actions).to(device), 1).detach()
-        old_logprobs = torch.squeeze(torch.stack(memory.logprobs), 1).to(device).detach()
+            # convert list to tensor
+            old_states = torch.tensor(memory.states).to(device).detach()
+            old_actions = torch.tensor(memory.actions).to(device).detach()[:, i, :]
+            old_logprobs = torch.tensor(memory.logprobs).to(device).detach()[:, i]
 
-        # Optimize policy for K epochs:
-        for _ in range(self.K_epochs):
-            # Evaluating old actions and values :
-            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+            # Optimize policy for K epochs:
+            for _ in range(self.K_epochs):
+                # Evaluating old actions and values :
+                logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions, i)
 
-            # Finding the ratio (pi_theta / pi_theta__old):
-            ratios = torch.exp(logprobs - old_logprobs.detach())
+                # Finding the ratio (pi_theta / pi_theta__old):
+                ratios = torch.exp(logprobs - old_logprobs.detach())
 
-            # Finding Surrogate Loss:
-            advantages = rewards - state_values.detach()
-            surr1 = ratios * advantages
-            surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
-            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
+                # Finding Surrogate Loss:
+                advantages = rewards - state_values.detach()
+                surr1 = ratios * advantages
+                surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
+                loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
 
-            # take gradient step
-            self.optimizer.zero_grad()
-            loss.mean().backward()
-            self.optimizer.step()
+                # take gradient step
+                self.optimizer.zero_grad()
+                loss.mean().backward()
+                self.optimizer.step()
 
         # Copy new weights into old policy:
         self.policy_old.load_state_dict(self.policy.state_dict())
-
-
-def main():
-    ############## Hyperparameters ##############
-    env_name = "BipedalWalker-v3"
-    exp_name = "ppo_continuous_check"
-    render = False
-    solved_reward = 300  # stop training if avg_reward > solved_reward
-    log_interval = 20  # print avg reward in the interval
-    max_episodes = 10000  # max training episodes
-    max_timesteps = 1500  # max timesteps in one episode
-
-    update_timestep = 4000  # update policy every n timesteps
-    action_std = 0.5  # constant std for action distribution (Multivariate Normal)
-    K_epochs = 80  # update policy for K epochs
-    eps_clip = 0.2  # clip parameter for PPO
-    gamma = 0.99  # discount factor
-
-    lr = 0.0003  # parameters for Adam optimizer
-    betas = (0.9, 0.999)
-
-    random_seed = None
-    #############################################
-
-    writer = SummaryWriter(logdir=os.path.join("logs/ppo_/", exp_name))
-    # creating environment
-    env = gym.make(env_name)
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
-
-    if random_seed:
-        print("Random Seed: {}".format(random_seed))
-        torch.manual_seed(random_seed)
-        env.seed(random_seed)
-        np.random.seed(random_seed)
-
-    memory = Memory()
-    ppo = PPO(state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip)
-    print(lr, betas)
-
-    # logging variables
-    running_reward = 0
-    avg_length = 0
-    time_step = 0
-
-    # training loop
-    for i_episode in range(1, max_episodes + 1):
-        state = env.reset()
-        t = 0
-        for t in range(max_timesteps):
-            time_step += 1
-            # Running policy_old:
-            action = ppo.select_action(state, memory)
-            state, reward, done, _ = env.step(action)
-
-            # Saving reward and is_terminals:
-            memory.rewards.append(reward)
-            memory.is_terminals.append(done)
-
-            # update if its time
-            if time_step % update_timestep == 0:
-                ppo.update(memory)
-                memory.clear_memory()
-                time_step = 0
-            running_reward += reward
-            if render:
-                env.render()
-            if done:
-                break
-
-        avg_length += t
-
-        # stop training if avg_reward > solved_reward
-        if running_reward > (log_interval * solved_reward):
-            print("########## Solved! ##########")
-            torch.save(ppo.policy.state_dict(), os.path.join("../../Desktop/others/ccdb_backup/plots/save-dir/ppo_/", exp_name))
-            break
-
-        # save every 500 episodes
-        if i_episode % 500 == 0:
-            if not os.path.exists(os.path.join("../../Desktop/others/ccdb_backup/plots/save-dir/ppo_/", exp_name)):
-                os.makedirs(os.path.join("../../Desktop/others/ccdb_backup/plots/save-dir/ppo_/", exp_name))
-            torch.save(ppo.policy.state_dict(), os.path.join("../../Desktop/others/ccdb_backup/plots/save-dir/ppo_/", exp_name))
-
-        # logging
-        if i_episode % log_interval == 0:
-            avg_length = int(avg_length / log_interval)
-            running_reward = int((running_reward / log_interval))
-
-            writer.add_scalar('Running Reward', running_reward, i_episode)
-            writer.add_scalar('Average Length', avg_length, i_episode)
-
-            print('Episode {} \t Avg length: {} \t Avg reward: {}'.format(i_episode, avg_length, running_reward))
-            running_reward = 0
-            avg_length = 0
-
-
-if __name__ == '__main__':
-    main()
-
--- multiwalker.py
+++ multiwalker.py
@@ -19,8 +19,8 @@ parser.add_argument("--load", type=bool, default=False, help="load true / false"
 args = parser.parse_args()
 config = read_config(args.config)
 
-n_walkers = config["main"]["n_walkers"]
-parallel_env = multiwalker_v5.parallel_env(n_walkers=n_walkers, position_noise=1e-3, angle_noise=1e-3,
+n_agents = config["main"]["n_walkers"]
+parallel_env = multiwalker_v5.parallel_env(n_walkers=n_agents, position_noise=1e-3, angle_noise=1e-3,
                                 forward_reward=1.0, fall_reward=-100.0, terminate_reward=-100.0,
                                 terminate_on_fall=True, max_cycles=500)
 
@@ -37,7 +37,7 @@ if random_seed:
     np.random.seed(random_seed)
 
 writer = SummaryWriter(logdir=os.path.join(config["main"]["logdir"], config["main"]["exp_name"]))
-local_memory = [Memory() for _ in range(n_walkers)]
+local_memory = [Memory() for _ in range(n_agents)]
 global_memory = Memory()
 
 betas = (0.9, 0.999)
@@ -55,8 +55,9 @@ local_agent = Local_Agent(
         hidden_nodes=config["global"]["hidden_nodes"]
     )
 global_messenger = Global_Messenger(
-        state_dim=(obs_dim * n_walkers) + (action_dim * n_walkers),  # all local observations concatenated + all agents' previous actions
-        action_dim=config["main"]["message_len"] * n_walkers,
+        state_dim=(obs_dim * n_agents) + (action_dim * n_agents),  # all local observations concatenated + all agents' previous actions
+        action_dim=config["main"]["message_len"],
+        n_agents=n_agents,
         action_std=config["global"]["action_std"],
         lr=config["global"]["lr"],
         betas=betas,
@@ -69,7 +70,7 @@ global_messenger = Global_Messenger(
 obs = parallel_env.reset()
 global_agent_state = [obs[i] for i in obs]
 global_agent_state = np.array(global_agent_state).reshape((-1,))
-global_agent_state = np.concatenate([global_agent_state, np.random.uniform(-1, 1, action_dim * n_walkers)])
+global_agent_state = np.concatenate([global_agent_state, np.random.uniform(-1, 1, action_dim * n_agents)])
 i_episode = 0
 episode_rewards = 0
 agents = [agent for agent in parallel_env.agents]
@@ -78,7 +79,7 @@ agents = [agent for agent in parallel_env.agents]
 for timestep in count(1):
     # Before running through each agent, we get messages for them, from the global agent.
     global_agent_output, global_agent_log_prob = global_messenger.select_action(global_agent_state)
-    global_agent_output = np.array(global_agent_output).reshape(n_walkers, -1)
+    global_agent_output = np.array(global_agent_output).reshape(n_agents, -1) ###################################################################! THIS STATEMENT
     
 
     for i, agent in enumerate(agents):
@@ -100,14 +101,14 @@ for timestep in count(1):
         local_memory[i].rewards.append(rewards[agent])
         local_memory[i].is_terminals.append(is_terminals[agent])
         # So essentially, episode_rewards will be a sum of all agent's rewards at each time step.
-        # And finally, we save the epside's performance by saving episode_rewards/n_walkers
+        # And finally, we save the epside's performance by saving episode_rewards/n_agents
         episode_rewards += rewards[agent]
 
     global_memory.states.append(global_agent_state)
     global_memory.actions.append(global_agent_output)
     global_memory.logprobs.append(global_agent_log_prob)
-    # global_memory.rewards.append()
-    # global_memory.is_terminals.append()
+    global_memory.rewards.append([rewards[agent] for agent in agents])
+    global_memory.is_terminals.append([is_terminals[agent] for agent in agents])
 
 
     # update if its time
@@ -115,18 +116,19 @@ for timestep in count(1):
         local_agent.update(local_memory)
         [mem.clear_memory() for mem in local_memory]
 
-    # if timestep % config["global"]["update_timestep"] == 0:
-    #     global_messenger.update(global_memory)
-    #     global_memory.clear_memory()
+    if timestep % config["global"]["update_timestep"] == 0:
+        global_messenger.update(global_memory)
+        global_memory.clear_memory()
 
     obs = next_obs
 
+    # If episode had ended
     if all([is_terminals[agent] for agent in agents]):
         i_episode += 1
-        writer.add_scalar('Avg reward for each agent, after an episode', episode_rewards/n_walkers, i_episode)
+        writer.add_scalar('Avg reward for each agent, after an episode', episode_rewards/n_agents, i_episode)
         timestep=0
         obs = parallel_env.reset()
-        print('\nEpisode {} \t  Avg reward for each agent, after an episode: {}\n'.format(i_episode, episode_rewards/n_walkers))
+        print('\nEpisode {} \t  Avg reward for each agent, after an episode: {}\n'.format(i_episode, episode_rewards/n_agents))
         episode_rewards = 0
 
     # save every 50 episodes
