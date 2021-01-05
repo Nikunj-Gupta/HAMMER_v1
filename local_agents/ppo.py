477c997d309a2f077db07a6fe46c5ddbd7e32850
nishantkr18 <nniishantkumar@gmail.com>
2021-01-05 21:16:15 +0530

    local models updated

--- local_agents/ppo.py
+++ local_agents/ppo.py
@@ -99,43 +99,44 @@ class PPO:
     def load(self, filename):
         self.policy.load_state_dict(torch.load(filename))
 
-    def update(self, memory):
-        # Monte Carlo estimate of rewards:
-        rewards = []
-        discounted_reward = 0
-        for reward, is_terminal in zip(reversed(memory.rewards), reversed(memory.is_terminals)):
-            if is_terminal:
-                discounted_reward = 0
-            discounted_reward = reward + (self.gamma * discounted_reward)
-            rewards.insert(0, discounted_reward)
+    def update(self, local_memory):
+        for i in range(len(local_memory)):
+            # Monte Carlo estimate of rewards:
+            rewards = []
+            discounted_reward = 0
+            for reward, is_terminal in zip(reversed(local_memory[i].rewards), reversed(local_memory[i].is_terminals)):
+                if is_terminal:
+                    discounted_reward = 0
+                discounted_reward = reward + (self.gamma * discounted_reward)
+                rewards.insert(0, discounted_reward)
 
-        # Normalizing the rewards:
-        rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+            # Normalizing the rewards:
+            rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
+            rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
 
-        # convert list to tensor
-        old_states = torch.squeeze(torch.stack(memory.states).to(device), 1).detach()
-        old_actions = torch.squeeze(torch.stack(memory.actions).to(device), 1).detach()
-        old_logprobs = torch.squeeze(torch.stack(memory.logprobs), 1).to(device).detach()
+            # convert list to tensor
+            old_states = torch.tensor(local_memory[i].states).to(device).detach()
+            old_actions = torch.tensor(local_memory[i].actions).to(device).detach()
+            old_logprobs = torch.tensor(local_memory[i].logprobs).to(device).detach()
 
-        # Optimize policy for K epochs:
-        for _ in range(self.K_epochs):
-            # Evaluating old actions and values :
-            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+            # Optimize policy for K epochs:
+            for _ in range(self.K_epochs):
+                # Evaluating old actions and values :
+                logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
 
-            # Finding the ratio (pi_theta / pi_theta__old):
-            ratios = torch.exp(logprobs - old_logprobs.detach())
+                # Finding the ratio (pi_theta / pi_theta__old):
+                ratios = torch.exp(logprobs - old_logprobs.detach())
 
-            # Finding Surrogate Loss:
-            advantages = rewards - state_values.detach()
-            surr1 = ratios * advantages
-            surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
-            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
+                # Finding Surrogate Loss:
+                advantages = rewards - state_values.detach()
+                surr1 = ratios * advantages
+                surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
+                loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
 
-            # take gradient step
-            self.optimizer.zero_grad()
-            loss.mean().backward()
-            self.optimizer.step()
+                # take gradient step
+                self.optimizer.zero_grad()
+                loss.mean().backward()
+                self.optimizer.step()
 
         # Copy new weights into old policy:
         self.policy_old.load_state_dict(self.policy.state_dict())
--- multiwalker.py
+++ multiwalker.py
@@ -4,7 +4,7 @@ import argparse
 from pettingzoo.sisl import multiwalker_v5
 import numpy as np
 
-from ppo_cont import PPO as Local_Agent
+from local_agents.ppo import PPO as Local_Agent
 from global_messenger.ppo import PPO as Global_Messenger
 from ppo import Memory
 from utils import read_config
@@ -72,75 +72,62 @@ global_agent_state = np.array(global_agent_state).reshape((-1,))
 global_agent_state = np.concatenate([global_agent_state, np.random.uniform(-1, 1, action_dim * n_walkers)])
 i_episode = 0
 episode_rewards = 0
+agents = [agent for agent in parallel_env.agents]
 
-
-# Training loop:
-for timestep in count(0):
+# Training loop, starting count from 1:
+for timestep in count(1):
     # Before running through each agent, we get messages for them, from the global agent.
-    global_agent_output = global_messenger.select_action(global_agent_state, global_memory)
+    global_agent_output, global_agent_log_prob = global_messenger.select_action(global_agent_state)
     global_agent_output = np.array(global_agent_output).reshape(n_walkers, -1)
     
 
-    for i, agent in enumerate(parallel_env.agents):
+    for i, agent in enumerate(agents):
         # "MAIN" decides whether the local agents take in any input from the global agent.
         local_state = np.concatenate([obs[agent], global_agent_output[i]]) if MAIN else obs[agent]
         local_action, local_log_prob = local_agent.select_action(local_state)
 
-        # Storing in states, actions, logprobs in memory:
+        # Storing in states, actions, logprobs in local_memory:
         local_memory[i].states.append(local_state)
         local_memory[i].actions.append(local_action)
         local_memory[i].logprobs.append(local_log_prob)
     
     # Creating a format for the actions in parallel_env:
-    actions = {agent : local_memory[i].actions[-1] for i, agent in enumerate(parallel_env.agents)}
+    actions = {agent : local_memory[i].actions[-1] for i, agent in enumerate(agents)}
     next_obs, rewards, is_terminals, infos = parallel_env.step(actions)
 
-    # Storing in rewards, is_terminals in memory:
-    for i, agent in enumerate(parallel_env.agents):
+    # Storing in rewards, is_terminals in local_memory, :
+    for i, agent in enumerate(agents):
         local_memory[i].rewards.append(rewards[agent])
         local_memory[i].is_terminals.append(is_terminals[agent])
         # So essentially, episode_rewards will be a sum of all agent's rewards at each time step.
         # And finally, we save the epside's performance by saving episode_rewards/n_walkers
         episode_rewards += rewards[agent]
 
-    global_memory.rewards.append(reward)
-    global_memory.is_terminals.append(done)
+    global_memory.states.append(global_agent_state)
+    global_memory.actions.append(global_agent_output)
+    global_memory.logprobs.append(global_agent_log_prob)
+    # global_memory.rewards.append()
+    # global_memory.is_terminals.append()
 
-    global_agent_next_state.append(obs)
-
-
-    if agent_number == n_walkers:
-        global_agent_next_state = np.array(global_agent_next_state).reshape((-1,))
-        prev_actions = np.array(prev_actions).reshape((-1,))
-        global_agent_next_state = np.concatenate([global_agent_next_state, prev_actions])
-        global_agent_state = global_agent_next_state
-        global_agent_next_state = []
-        prev_actions = []
 
     # update if its time
     if timestep % config["local"]["update_timestep"] == 0:
         local_agent.update(local_memory)
-        local_memory.clear_memory()
+        [mem.clear_memory() for mem in local_memory]
 
-    if timestep % config["global"]["update_timestep"] == 0:
-        global_messenger.update(global_memory)
-        global_memory.clear_memory()
+    # if timestep % config["global"]["update_timestep"] == 0:
+    #     global_messenger.update(global_memory)
+    #     global_memory.clear_memory()
 
     obs = next_obs
 
-    is_terminals = [parallel_env.is_terminals[a] for a in parallel_env.agents]
-    if not parallel_env.agents:
+    if all([is_terminals[agent] for agent in agents]):
         i_episode += 1
         writer.add_scalar('Avg reward for each agent, after an episode', episode_rewards/n_walkers, i_episode)
         timestep=0
         obs = parallel_env.reset()
-        global_agent_next_state = []
-        prev_actions = []
         print('\nEpisode {} \t  Avg reward for each agent, after an episode: {}\n'.format(i_episode, episode_rewards/n_walkers))
         episode_rewards = 0
-
-    if i_episode == config["main"]["max_episodes"]:
-        break
 
     # save every 50 episodes
     if i_episode % config["main"]["save_interval"] == 0:
@@ -150,5 +137,14 @@ for timestep in count(0):
                    os.path.join(config["main"]["save_dir"], config["main"]["exp_name"], "local_agent.pth"))
         torch.save(global_messenger.policy.state_dict(),
                    os.path.join(config["main"]["save_dir"], config["main"]["exp_name"], "global_messenger.pth"))
-
+    
+    if i_episode == config["main"]["max_episodes"]:
+        break
+    
+    # Update global_agent_state here:
+    # Note: we use obs here, to insure that if the episode ends, we give the global agent, the new reset state.
+    global_agent_state = np.array([obs[agent] for agent in agents]).reshape((-1,))
+    prev_actions = np.array([actions[agent] for agent in agents]).reshape((-1,))
+    global_agent_state = np.concatenate([global_agent_state, prev_actions])
+    global_agent_state = global_agent_state
 
