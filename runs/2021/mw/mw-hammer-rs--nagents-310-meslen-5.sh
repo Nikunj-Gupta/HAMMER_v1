fa813c4accc994511545eb49836a1175d1ae41c9
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-01-08 00:36:53 +0530

    run files for MW

---
+++ runs/2021/mw/mw-hammer-rs--nagents-310-meslen-5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=00-30:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+python ../../../multiwalker.py --config ../../../configs/2021/mw/hyperparameters.yaml --hammer 1 --expname mw-hammer-rs--nagents-310-meslen-5 nwalkers 3 --maxepisodes 100000 --randomseed 10 --meslen 5
