a76b993afd96877394121e9d3fa452967c6adef0
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-01-07 23:26:11 +0530

    removed older run file

--- runs/2021/cn/sample_run_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:2
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=127000M
-#SBATCH --time=48:00:00
-#SBATCH --account=def-mtaylor3
-tensorboard --logdir=logs/ --host 0.0.0.0 &
-python3 ../../../main_complete_state_and_prev_actions.py  --config ../../../configs/2021/cn/hyperparams.yaml --expname test --hammer 1 --maxepisodes 50000 --meslen 2 
