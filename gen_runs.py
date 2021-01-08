d0268a5244be28d5509bf6eefe4781c395526ee0
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-01-08 21:48:22 +0530

    updated to take in nagents for CN too

--- gen_runs.py
+++ gen_runs.py
@@ -1,73 +1,23 @@
-import os, glob
+import os 
 
 
-# ==============================================================================
-# Hammer and IL on Cooperative Navigation 
-# ==============================================================================
+codes = {
+    "CN": {
+        "script": "../../../main_complete_state_and_prev_actions.py", 
+        "config": "../../../configs/2021/cn/hyperparams.yaml", 
+        "dumpdir": "runs/2021/test", 
+        "maxepisodes": 100000
+    }, 
 
-# code = "../../../main_complete_state_and_prev_actions.py"
-# config = "../../../configs/2021/cn/hyperparams.yaml" 
-
-# fixed_text = "#!/bin/bash\n"\
-#                     "#SBATCH --nodes=1\n"\
-#                     "#SBATCH --gres=gpu:2\n"\
-#                     "#SBATCH --ntasks-per-node=32\n"\
-#                     "#SBATCH --mem=127000M\n"\
-#                     "#SBATCH --time=00-30:00\n"\
-#                     "#SBATCH --account=def-mtaylor3\n"\
-#                     "tensorboard --logdir=logs/ --host 0.0.0.0 &\n" 
-
-# for seed in range(9, 11): 
-#     for meslen in range(4, 6): 
-#         expname = "cn-hammer-nagents-3-rs-"+str(seed)+"-meslen-"+str(meslen) 
-#         command = " ".join([
-#             "python", code, 
-#             "--config", config, 
-#             "--hammer", str(hammer), 
-#             "--expname", expname, 
-#             "--maxepisodes", "100000", 
-#             "--randomseed", str(seed), 
-#             "--meslen", str(meslen)
-#         ]) 
-#         with open(os.path.join("runs/2021/cn", expname + ".sh"), "w") as f:
-#             f.write(fixed_text + command)
+    "MW": {
+        "script": "../../../multiwalker.py", 
+        "config": "../../../configs/2021/mw/hyperparameters.yaml", 
+        "dumpdir": "runs/2021/test", 
+        "maxepisodes": 100000 
+    }
+}
 
 
-# ==============================================================================
-# IL on Cooperative Navigation 
-# ==============================================================================
-
-
-# fixed_text = "#!/bin/bash\n"\
-#                     "#SBATCH --nodes=1\n"\
-#                     "#SBATCH --gres=gpu:2\n"\
-#                     "#SBATCH --ntasks-per-node=32\n"\
-#                     "#SBATCH --mem=127000M\n"\
-#                     "#SBATCH --time=00-06:00\n"\
-#                     "#SBATCH --account=def-mtaylor3\n"\
-#                     "tensorboard --logdir=logs/ --host 0.0.0.0 &\n" 
-
-
-# for seed in range(9, 11): 
-#     expname = "cn-il-nagents-3--rs-"+str(seed) 
-#     command = " ".join([
-#         "python", code, 
-#         "--config", config, 
-#         "--hammer", "0", 
-#         "--expname", expname, 
-#         "--maxepisodes", "100000", 
-#         "--randomseed", str(seed)
-#     ]) 
-
-#     with open(os.path.join("runs/2021/cn", expname + ".sh"), "w") as f:
-#         f.write(fixed_text + command)
-
-# ==============================================================================
-# Hammer on Multi-Agent Walker  
-# ==============================================================================
-
-code = "../../../multiwalker.py"
-config = "../../../configs/2021/mw/hyperparameters.yaml" 
 
 fixed_text = "#!/bin/bash\n"\
                     "#SBATCH --nodes=1\n"\
@@ -78,40 +28,25 @@ fixed_text = "#!/bin/bash\n"\
                     "#SBATCH --account=def-mtaylor3\n"\
                     "tensorboard --logdir=logs/ --host 0.0.0.0 &\n" 
 
-for seed in range(9, 11): 
-    for meslen in range(4, 6): 
-        for nagent in range(3, 6, 2): 
-            expname = "mw-hammer-rs-"+"-nagents-"+str(nagent)+str(seed)+"-meslen-"+str(meslen) 
-            command = " ".join([
-                "python", code, 
-                "--config", config, 
-                "--hammer", "1", 
-                "--expname", expname, 
-                "--nwalkers", str(nagent), 
-                "--maxepisodes", "100000", 
-                "--randomseed", str(seed), 
-                "--meslen", str(meslen)
-            ]) 
-            with open(os.path.join("runs/2021/mw", expname + ".sh"), "w") as f:
-                f.write(fixed_text + command)
 
 
+for code in codes:  
+    for hammer in [0, 1]: 
+        for nagents in [3, 5]: 
+            for seed in range(9, 11): 
+                for meslen in range(4, 6): 
+                    expname = "hammer" if hammer else "IL" 
+                    expname = code + "-" + expname + "-nagents-" + str(nagents) + "-rs-" + str(seed)+ "-meslen-" + str(meslen) 
+                    command = " ".join([
 
-# ==============================================================================
-# IL on Multi-Agent Walker  
-# ==============================================================================
-
-for seed in range(9, 11): 
-    for nagent in range(3, 6, 2): 
-        expname = "mw-il-nagents-"+str(nagent)+"-rs-"+str(seed) 
-        command = " ".join([
-            "python", code, 
-            "--config", config, 
-            "--hammer", "0", 
-            "--expname", expname, 
-            "--nwalkers", str(nagent), 
-            "--maxepisodes", "100000", 
-            "--randomseed", str(seed), 
-        ]) 
-        with open(os.path.join("runs/2021/mw", expname + ".sh"), "w") as f:
-            f.write(fixed_text + command)
+                        "python", codes[code]['script'], 
+                        "--config", codes[code]['config'], 
+                        "--hammer", str(hammer), 
+                        "--expname", expname, 
+                        "--nagents", str(nagents), 
+                        "--maxepisodes", str(codes[code]["maxepisodes"]), 
+                        "--randomseed", str(seed), 
+                        "--meslen", str(meslen)
+                    ]) 
+                    with open(os.path.join(codes[code]["dumpdir"], expname + ".sh"), "w") as f:
+                        f.write(fixed_text + command)
