730e2cb96d17e721f585d9f82e1c09bdbb8f55dc
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-01-08 21:50:13 +0530

    generated new run files for further testing on compute canada

---
+++ runs/2021/test/CN-IL-nagents-3-rs-9-meslen-5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=00-30:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+python ../../../main_complete_state_and_prev_actions.py --config ../../../configs/2021/cn/hyperparams.yaml --hammer 0 --expname CN-IL-nagents-3-rs-9-meslen-5 --nagents 3 --maxepisodes 100000 --randomseed 9 --meslen 5
---
+++ runs/2021/test/MW-hammer-nagents-5-rs-10-meslen-4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=00-30:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+python ../../../multiwalker.py --config ../../../configs/2021/mw/hyperparameters.yaml --hammer 1 --expname MW-hammer-nagents-5-rs-10-meslen-4 --nagents 5 --maxepisodes 100000 --randomseed 10 --meslen 4
