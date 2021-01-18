b58c35ed371c8a4215f87f87ddf836403da38aae
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-01-18 21:44:19 +0530

    moved older runs

--- runs/2021/mw/mw-il-nagents-3-rs-9.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:2
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=127000M
-#SBATCH --time=00-30:00
-#SBATCH --account=def-mtaylor3
-tensorboard --logdir=logs/ --host 0.0.0.0 &
-python ../../../multiwalker.py --config ../../../configs/2021/mw/hyperparameters.yaml --hammer 0 --expname mw-il-nagents-3-rs-9 --nwalkers 3 --maxepisodes 100000 --randomseed 9
