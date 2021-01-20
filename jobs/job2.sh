760282492ac744cf71d2aa2149acc5fab5bb6f3c
nishantkr18 <nniishantkumar@gmail.com>
2021-01-20 22:55:32 +0530

    updated runs

--- jobs/job2.sh
+++ jobs/job2.sh
@@ -3,4 +3,4 @@
 #SBATCH --time=00-10:00
 #SBATCH --account=def-mtaylor3
 tensorboard --logdir=logs/ --host 0.0.0.0 &
-python ../hammer-cn.py --config ../configs/2021/cn/hyperparams.yaml --maxepisodes 500000 --expname hammer-prevactionsno--partialobsyes--sharedparamsno--meslen4 --hammer 1 --randomseed 685 --meslen 4 --prevactions 0 --partialobs 1 --sharedparams 0
+python ../hammer-cn.py --config ../configs/2021/cn/hyperparams.yaml --maxepisodes 500000 --expname hammer-prevactionsno--partialobsyes--sharedparamsno--meslen4--discretemes0 --discretemes 0 --hammer 1 --randomseed 685 --meslen 4 --prevactions 0 --partialobs 1 --sharedparams 0
