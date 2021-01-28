7c86b9fe65d00bdfd6e8fb65506b4cbf80c4f88e
nishantkr18 <nniishantkumar@gmail.com>
2021-01-28 18:21:50 +0530

    updated run

--- runs/2021/SMARTS/IL.sh
+++ runs/2021/SMARTS/IL.sh
@@ -2,5 +2,9 @@
 #SBATCH --mem=5000M
 #SBATCH --time=00-10:00
 #SBATCH --account=def-mtaylor3
+module load StdEnv/2020 python/3.7.9 sumo/1.7.0 geos/3.8.1
+export SUMO_HOME=$EBROOTSUMO
+source ~/VENV/bin/activate
+
 tensorboard --logdir=logs/ --host 0.0.0.0 &
 time python ../../../hammer-smarts.py --config ../../../configs/2021/cn/hyperparams.yaml --hammer 0 --render False
