0930124c69663f6219cd11fda16501dc7ede3189
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-02-02 00:24:30 +0530

    reduced runs

---
+++ runs/2021/sharedparamsyes/hammer-prevactionsno--partialobsyes--sharedparamsyes--heterogeneityno--discretemes--rs984--meslen4-.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-8:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-cn.py --config ../../../configs/2021/cn/hyperparams.yaml --maxepisodes 100000 --expname hammer-prevactionsno--partialobsyes--sharedparamsyes--heterogeneityno--discretemes--rs984--meslen4- --hammer 1 --randomseed 984 --discretemes 1 --meslen 4 --prevactions 0 --partialobs 1 --sharedparams 1 --heterogeneity 0
---
+++ runs/2021/sharedparamsyes/hammer-prevactionsyes--partialobsno--sharedparamsyes--heterogeneityyes--continuousmes--rs465--meslen4-.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-8:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-cn.py --config ../../../configs/2021/cn/hyperparams.yaml --maxepisodes 100000 --expname hammer-prevactionsyes--partialobsno--sharedparamsyes--heterogeneityyes--continuousmes--rs465--meslen4- --hammer 1 --randomseed 465 --discretemes 0 --meslen 4 --prevactions 1 --partialobs 0 --sharedparams 1 --heterogeneity 1
