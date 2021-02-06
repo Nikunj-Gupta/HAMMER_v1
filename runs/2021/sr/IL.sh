221d433bd643f0e0e111163d698962d24cb1cdea
nishantkr18 <nniishantkumar@gmail.com>
2021-02-06 19:15:48 +0530

    added sr

---
+++ runs/2021/sr/IL.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-7:30
+#SBATCH --account=def-mtaylor3
+source ~/VENV/bin/activate
+
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-sr.py --config ../../../configs/2021/cn/hyperparams.yaml --expname IL-sr --hammer 0
