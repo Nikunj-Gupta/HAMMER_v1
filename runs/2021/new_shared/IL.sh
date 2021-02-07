9d89cb1b9aea59c576dcb48bbc52619e0ceec55d
nishantkr18 <nniishantkumar@gmail.com>
2021-02-07 13:20:42 +0530

    added run files

---
+++ runs/2021/new_shared/IL.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-8:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-cn.py --config ../../../configs/2021/cn/hyperparams.yaml --hammer 0
