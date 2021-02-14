daa83cbe43a98019bc0de35d3fc891bd9e70d209
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-02-15 01:14:53 +0530

    added basline runs for sr env

---
+++ runs/2021/messagegradients_new/ILsharedparams--sr--partialobs1--rs984.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../il.py --config ../../../configs/2021/cn/hyperparams.yaml --expname ILsharedparams--sr--partialobs1--rs984 --partialobs 1 --randomseed 984 --nagents 2 --limit 11
