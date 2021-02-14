3032ece3b0ccbb6c2ba1bba73a195e99ee502ca2
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-02-14 18:40:42 +0530

    added more test runs

---
+++ runs/2021/messagegradients_new/Hammer_previous--cn--partialobs0--rs984--meslen1--discretemes0.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-cn.py --config ../../../configs/2021/cn/hyperparams.yaml --expname Hammer_previous--cn--partialobs0--rs984--meslen1--discretemes0 --partialobs 0 --randomseed 984 --nagents 3 --meslen 1 --discretemes 0
