270632f03abfa6f9e3bcd65568d5a7229cb7a6d6
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-02-16 21:58:24 +0530

    added runs for centralized training

---
+++ runs/2021/message_gradients_heterogeneity_rs/centralized_training--cn--rs674.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../centralized_training.py --config ../../../configs/2021/cn/hyperparams.yaml --expname centralized_training--cn--rs674 --randomseed 674 --nagents 3
