6020596c8c42fea1c6ff2e685a566ae3968c539a
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-02-18 21:23:52 +0530

    added runs

---
+++ runs/2021/mw_experiments/il_multiwalker--mw--rs354.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-10:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../il_multiwalker.py --config ../../../configs/2021/cn/hyperparams.yaml --expname il_multiwalker--mw--rs354 --randomseed 354
