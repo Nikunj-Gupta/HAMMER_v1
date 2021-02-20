73e1a65d4864e5814dee4488cf9fa0d19c404137
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-02-20 14:36:19 +0530

    added additional runs for mw for checking

---
+++ runs/2021/mw_new/Hammerwithgradients_mw--mw--meslen1--dru--rs234.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-48:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-with-gradient-mw.py --config ../../../configs/2021/cn/hyperparams.yaml --expname Hammerwithgradients_mw--mw--meslen1--dru--rs234 --maxepisodes 50000 --randomseed 234 --meslen 1 --dru_toggle 1
---
+++ runs/2021/mw_new/il_multiwalker--mw--rs674.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-48:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../il_multiwalker.py --config ../../../configs/2021/cn/hyperparams.yaml --expname il_multiwalker--mw--rs674 --maxepisodes 50000 --randomseed 674
