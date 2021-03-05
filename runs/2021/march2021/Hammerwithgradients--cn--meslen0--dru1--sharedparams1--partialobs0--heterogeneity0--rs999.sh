54376fa521512ecfbc8832d5e12f9ea01b78507a
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-03-05 16:45:36 +0530

    fresh runs

---
+++ runs/2021/march2021/Hammerwithgradients--cn--meslen0--dru1--sharedparams1--partialobs0--heterogeneity0--rs999.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-with-gradient.py --config ../../../configs/2021/cn/hyperparams.yaml --expname Hammerwithgradients--cn--meslen0--dru1--sharedparams1--partialobs0--heterogeneity0--rs999 --partialobs 0 --heterogeneity 0 --dru_toggle 1 --meslen 0 --randomseed 999 --sharedparams 1
---
+++ runs/2021/march2021/Hammerwithgradients--cn--meslen1--dru1--sharedparams1--partialobs0--heterogeneity1--rs999.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-with-gradient.py --config ../../../configs/2021/cn/hyperparams.yaml --expname Hammerwithgradients--cn--meslen1--dru1--sharedparams1--partialobs0--heterogeneity1--rs999 --partialobs 0 --heterogeneity 1 --dru_toggle 1 --meslen 1 --randomseed 999 --sharedparams 1
