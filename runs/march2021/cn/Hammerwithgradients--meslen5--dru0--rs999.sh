5cab32e3bb9b28e5983195655109b87f5fedc1d4
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-03-07 20:50:55 +0530

    added runs

---
+++ runs/march2021/cn/Hammerwithgradients--meslen5--dru0--rs999.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-with-gradient.py --config ../../../configs/2021/cn/hyperparams.yaml --expname Hammerwithgradients--meslen5--dru0--rs999 --dru_toggle 0 --meslen 5 --randomseed 999
---
+++ runs/march2021/cn/Hammerwithgradients--meslen5--dru1--rs999.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --mem=5000M
+#SBATCH --time=00-16:00
+#SBATCH --account=def-mtaylor3
+tensorboard --logdir=logs/ --host 0.0.0.0 &
+time python ../../../hammer-with-gradient.py --config ../../../configs/2021/cn/hyperparams.yaml --expname Hammerwithgradients--meslen5--dru1--rs999 --dru_toggle 1 --meslen 25 --randomseed 999
